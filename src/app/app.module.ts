a1435288bbfb53a5b9ea30779768b1a6549f7d52
Aaron Cooper <aaron.cooper@2359media.com>
2017-05-27 13:21:18 +0800

    Added basic content and header/footer components. Added markdown-to-html module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MarkdownToHtmlModule } from 'ng2-markdown-to-html';
+
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MarkdownToHtmlModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.content.md
@@ -0,0 +1,12 @@
+# Introduction
+
+Thanks for viewing **Angular Guidebook**!
+
+This is an online resource that is designed to help you pick up the latest
+version of Angular (formally known as _Angular 2_) quickly, and teach you
+the best practices and advice needed to build scaleable Angular apps in as
+little time as possible.
+
+Whilst every effort has been made by it's creator to ensure the information
+contained is up to date, easy to understand and above all _correct_, please
+don't hesitate to reach out if you find any errors or inconsistencies.
