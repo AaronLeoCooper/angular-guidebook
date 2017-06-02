7b541b7b5a9384a96f2d0c1b13e36e1897fb7b87
Aaron Cooper <aaron.cooper@2359media.com>
2017-06-02 22:12:01 +0800

    Added SPA express server, added forever for self-reviving server.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const historyApiFallback = require('express-history-api-fallback');
+
+const port = process.env.PORT || 8080;
+const root = path.join(__dirname, '../', 'dist');
+
+const App = express();
+
+App.use(morgan('tiny'));
+App.use(express.static(root));
+App.use(historyApiFallback('index.html', { root }));
+
+App.listen(port, function () {
+  console.info(`~~~~~ Serving at port: ${port} ~~~~~`);
+});
